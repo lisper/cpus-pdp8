9d89453b0f2543422303912cdd25d63ca648e750
brad <brad@52cf671a-1126-0410-802d-a71460acc760>
2010-04-13 13:59:00 +0000

    added ushow for sim output

---
+++ utils/ushow/ushow.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int c;
+char b[1024];
+
+void add(int v)
+{
+	b[c++] = v & 0x7f;
+	b[c] = 0;
+	printf("output: %s\n", b);
+	fflush(stdout);
+}
+
+main()
+{
+	char line[1024];
+
+	while (fgets(line, sizeof(line), stdin)) {
+		if (memcmp(&line[4], "tx_data ", 8) == 0) {
+			char s1[256], s2[256];
+			int v;
+			sscanf(line, "%s %s %o", s1, s2, &v);
+			add(v);
+		}
+	}
+}
